bac636781d43298e8ae84fa468dedcda5b649489
ryanbikis <ryanbikis3@gmail.com>
2022-05-13 16:03:08 -0700

    testing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TherapistComponent } from './therapist/therapist.component';
+import { ListallComponent } from './therapist/listall/listall.component';
+import { ListoneComponent } from './therapist/listone/listone.component';
+import { EditComponent } from './therapist/edit/edit.component';
+import { CreateComponent } from './therapist/create/create.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TherapistComponent,
+    ListallComponent,
+    ListoneComponent,
+    EditComponent,
+    CreateComponent
   ],
   imports: [
     BrowserModule,
