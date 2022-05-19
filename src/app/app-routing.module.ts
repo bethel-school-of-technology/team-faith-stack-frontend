b4c8647ce5517e8f1b13270e929bc2f283f3a9ef
ryanbikis <ryanbikis3@gmail.com>
2022-05-19 12:50:26 -0700

    routes/imports

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,50 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddComponent } from './components/admin/add/add.component';
+import { DeleteComponent } from './components/admin/delete/delete.component';
+import { EditComponent } from './components/admin/edit/edit.component';
+import { ViewComponent } from './components/admin/view/view.component';
+import { CreateComponent } from './components/post/create/create.component';
+import { DeleteComponent } from './components/post/delete/delete.component';
+import { EditComponent } from './components/post/edit/edit.component';
+import { ListallComponent } from './components/post/listall/listall.component';
+import { CreateComponent } from './components/therapist/create/create.component';
+import { EditComponent } from './components/therapist/edit/edit.component';
+import { ListoneComponent } from './components/therapist/listone/listone.component';
+import { ListallComponent } from './components/therapist/listall/listall.component';
+import { EditComponent } from './components/user/edit/edit.component';
+import { PostComponent } from './components/user/post/post.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "admin/add", component: AddComponent },
+
+  { path: "admin/delete", component: DeleteComponent },
+
+  { path: "admin/edit", component: EditComponent},
+
+  { path: "admin/view", component: ViewComponent},
+
+  { path: "post/create", component: CreateComponent},
+
+  { path: "post/delete", component: DeleteComponent},
+
+  { path: "post/edit", component: EditComponent},
+
+  { path: "post/listall", component: ListallComponent},
+
+  { path: "therapist/create", component: CreateComponent},
+
+  { path: "therapist/edit", component: EditComponent},
+
+  { path: "therapist/listall", component: ListallComponent},
+
+  { path: "therapist/listone", component: ListoneComponent},
+
+  { path: "user/edit", component: EditComponent},
+
+  { path: "user/post", component: PostComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TherapistComponent } from './therapist/therapist.component';
-import { ListallComponent } from './therapist/listall/listall.component';
+import { TherapistComponent } from './components/therapist';
+import { ListallComponent } from '.therapist.components';
 import { ListoneComponent } from './therapist/listone/listone.component';
 import { EditComponent } from './therapist/edit/edit.component';
 import { CreateComponent } from './therapist/create/create.component';
