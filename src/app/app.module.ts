deb0ce9003b42e4bdcee55e41a5783b31641043b
Patrick Bruner <patrick.bruner@jessup.edu>
2022-05-22 13:32:15 -0400

    helped with app-routing.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ListallComponent } from './Components/listall.therapist/listall.component';
-import { ListoneComponent } from './Components/listone.therapist/listone.component';
-import { EditComponent } from './Components/edit.therapist/edit.component';
-import { CreateComponent } from './Components/create.therapist/create.component';
-import { DeleteComponent } from './post/delete/delete.component';
-import { ViewComponent } from './admin/view/view.component';
-import { AddComponent } from './admin/add/add.component';
+import { RouterModule, Routes } from '@angular/router';
+import { AddComponent } from './Components/admin/add.admin/add.component';
+import { DeleteComponent } from './Components/admin/delete.admin/delete.component';
+import { EditComponent } from './Components/admin/edit.admin/edit.component';
+import { ViewComponent } from './Components/admin/view.admin/view.component';
 import { LoginComponent } from './Components/login/login.component';
 import { RegisterComponent } from './Components/register/register.component';
-import { HttpClientModule } from '@angular/common/http';
+import { CreateComponent } from './Components/post/create.post/create.component';
+import { DeleteComponent } from './Components/post/delete.post/delete.component';
+import { EditComponent } from './Components/post/edit.post/edit.component';
+import { ListallComponent } from './Components/post/listall.post/listall.component';
+import { CreateComponent } from './Components/therapist/create.therapist/create.component';
+import { EditComponent } from './Components/therapist/edit.therapist/edit.component';
+import { ListallComponent } from './Components/therapist/listall.therapist/listall.component';
+import { ListoneComponent } from './Components/therapist/listone.therapist/listone.component';
+import { EditComponent } from './Components/user/edit.user/edit.component';
+import { PostComponent } from './Components/user/post.user/post.component';
+
 
 @NgModule({
   declarations: [
@@ -26,12 +34,13 @@ import { HttpClientModule } from '@angular/common/http';
     ViewComponent,
     AddComponent,
     LoginComponent,
-    RegisterComponent
-  ],
+    RegisterComponent],
+    
   imports: [
     BrowserModule,
-    AppRoutingModule
-    HttpClientModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
