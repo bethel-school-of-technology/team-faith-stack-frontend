e5e46b577c9914cb006ea1c021ae7d6fb2733ada
Patrick Bruner <patrick.bruner@jessup.edu>
2022-05-24 18:16:41 -0400

    fixing app.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,46 +1,52 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 
-import { RouterModule, Routes } from '@angular/router';
-import { AddComponent } from './Components/admin/add.admin/add.component';
-import { DeleteComponent } from './Components/admin/delete.admin/delete.component';
-import { EditComponent } from './Components/admin/edit.admin/edit.component';
-import { ViewComponent } from './Components/admin/view.admin/view.component';
-import { LoginComponent } from './Components/login/login.component';
-import { RegisterComponent } from './Components/register/register.component';
-import { CreateComponent } from './Components/post/create.post/create.component';
-import { DeleteComponent } from './Components/post/delete.post/delete.component';
-import { EditComponent } from './Components/post/edit.post/edit.component';
-import { ListallComponent } from './Components/post/listall.post/listall.component';
-import { CreateComponent } from './Components/create.therapist/create.component';
-import { EditComponent } from './Components/therapist/edit.therapist/edit.component';
-import { ListallComponent } from './Components/therapist/listall.therapist/listall.component';
-import { ListoneComponent } from './Components/therapist/listone.therapist/listone.component';
-import { EditComponent } from './Components/user/edit.user/edit.component';
-import { PostComponent } from './Components/user/post.user/post.component';
-
+import { Router, RouterModule } from '@angular/router';
+import { AdminAddComponent } from './components/admin/add/add.component';
+import { AdminDeleteComponent } from './components/admin/delete/delete.component';
+import { AdminEditComponent } from './components/admin/edit/edit.component';
+import { AdminViewComponent } from './components/admin/view/view.component';
+import { PostCreateComponent } from './components/post/create/create.component';
+import { PostDeleteComponent } from './components/post/delete/delete.component';
+import { PostEditComponent } from './components/post/edit/edit.component';
+import { PostListallComponent } from './components/post/listall/listall.component';
+import { TherapistCreateComponent } from './components/therapist/create/create.component';
+import { TherapistEditComponent } from './components/therapist/edit/edit.component';
+import { TherapistListoneComponent } from './components/therapist/listone/listone.component';
+import { TherapistListallComponent } from './components/therapist/listall/listall.component';
+import { UserEditComponent } from './components/user/edit/edit.component';
+import { UserPostComponent } from './components/user/post/post.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListallComponent,
-    ListoneComponent,
-    EditComponent,
-    CreateComponent,
-    PostComponent,
-    DeleteComponent,
-    ViewComponent,
-    AddComponent,
-    LoginComponent,
-    RegisterComponent],
-    
+    PostDeleteComponent,
+    PostListallComponent,
+    TherapistListoneComponent,
+    AdminEditComponent,
+    PostCreateComponent,
+    UserPostComponent,
+    AdminDeleteComponent,
+    AdminViewComponent,
+    AdminAddComponent,
+    PostEditComponent,
+    TherapistCreateComponent,
+    TherapistEditComponent,
+    TherapistListallComponent,
+    UserEditComponent
+  ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    FormsModule
+    AppRoutingModule, 
+    RouterModule,
+    Router,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
