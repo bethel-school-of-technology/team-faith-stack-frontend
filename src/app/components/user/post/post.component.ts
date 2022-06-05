511845db9d4a0b08f3d33c58cf1928d8d2f455b1
Patrick Bruner <patrick.bruner@jessup.edu>
2022-06-05 17:03:29 -0400

    profile fixes

--- src/app/components/user/post/post.component.ts
+++ src/app/components/user/post/post.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-// import { parse } from 'path';
-import { map, Subscription } from 'rxjs';
 import { user } from 'src/app/models/user/user.model';
 import { UsersService } from 'src/app/services/users.service';
 
@@ -13,17 +11,16 @@ import { UsersService } from 'src/app/services/users.service';
 export class UserPostComponent implements OnInit {
 
   userID: number = null;
-  private sub: Subscription;
   user: user = null;
 
 
   constructor(private actRoute: ActivatedRoute, private usersService: UsersService, private router: Router) { }
 
   ngOnInit(): void {
-    this.sub = this.actRoute.params.subscribe(params => {
+    this.actRoute.params.subscribe(params => {
       this.userID = parseInt(params['id']);
       this.usersService.findOne(this.userID).pipe(
-        map((user: user) => this.user = user)
+        //map((user: user) => this.user = user)
       ).subscribe()
     });
 
