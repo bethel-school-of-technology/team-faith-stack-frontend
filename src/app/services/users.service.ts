07fc56129253b3a09f405edd199626ae913a43e7
ryanbikis <ryanbikis3@gmail.com>
2022-06-08 18:45:51 -0700

    changes

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -13,11 +13,6 @@ import { post } from '../models/Post/post.model';
 
 export class UsersService implements CanActivate {
   
-/*   loadAllPosts() {
-    throw new Error('Method not implemented.');
-  }
- */
-
   // environment.apiBaseUrlUsers: 'http://localhost:8080'
   apiServerUrl: string = environment.apiBaseUrlUsers;
 
